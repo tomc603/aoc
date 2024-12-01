1905af64b60374bb6da481adf7338e3134d6af0c
Tom Cameron <tomc603@gmail.com>
2024-12-01 17:23:53 -0500

    Part 1 and 2 for day 1

---
+++ 2024/day-01/solution.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+
+input_file = open("input", "r")
+data=input_file.readlines()
+
+left = []
+right = []
+for l, r in [line.split() for line in data]:
+    # There's an easier way to do this, I just can't think of it.
+    left.append(int(l))
+    right.append(int(r))
+
+left.sort()
+right.sort()
+
+dist = sum(map(lambda l, r: abs(l-r), left, right))
+sim = sum(map(lambda l: l * right.count(l), left))
+print(f"Difference: {dist}")
+print(f"Similarity: {sim}")
