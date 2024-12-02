95a12321d719b65224e0bc7f1197df8cd1fdaa4f
Tom Cameron <tomc603@gmail.com>
2024-12-02 14:47:12 -0500

    Day 1, Part 2

--- 2024/day-02/solution.py
+++ 2024/day-02/solution.py
@@ -14,6 +14,17 @@ def safe(report: list):
             (all([1 <= abs(l) <= 3 for l in level_change]))
     )
 
+def dampened_safe(report: list):
+    # If the report is safe in the strict regime, short circuit and return immediately
+    if safe(report): return True
+
+    # Otherwise, check if any combination of levels is safe after removing a single levels value
+    return any([safe(levels) for levels in itertools.combinations(report, len(report) - 1)])
+
+
 reports = [[int(y) for y in x.split()] for x in open("input", "r").readlines()]
 safe_reports = sum([safe(report) for report in reports])
 print(f"Safe reports: {safe_reports}")
+
+dampened_reports = sum([dampened_safe(report) for report in reports])
+print(f"Dampened safe reports: {dampened_reports}")
