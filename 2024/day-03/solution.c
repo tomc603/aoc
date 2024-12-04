32fb2e388f69d2b00ac30c7cb937a6772f955837
Tom Cameron <tomc603@gmail.com>
2024-12-04 16:22:24 -0500

    Day 3 solution in C

--- 2024/day-03/solution.c
+++ 2024/day-03/solution.c
@@ -1,18 +1,129 @@
+#include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 
+// Maximum length of a valid instruction
+#define MAX_INSTRUCTION_LEN 8
+
+
+// State machine states for instruction processor
+enum parse_state {
+    parser_error = -1,
+    parser_none = 0,
+    parser_left_digit,
+    parser_comma,
+    parser_right_digit,
+    parser_close_paren,
+    parser_complete
+};
+
+
 // Forward declarations
-void parse_instructions(char *input);
+long parse_instructions(const char *input);
 void recover_instructions(const char *input, size_t len);
 
 
-void parse_instructions(char *input) {
-    // Check if ASCII character is 0-9: x > 47 && x < 58
-    // Use state variables and a switch statement for each character.
-    // The default case should reset the state variables.
+/**
+ * @brief Process input data searching for valid instruction sets, returning the sum of the instructions.
+ * @param input Data to search for valid instructions.
+ * @return Sum of the products from the valid instructions.
+ */
+long parse_instructions(const char *input) {
+    char token[] = "mul(";
+    long total = 0;
+
+    char *instruction = strstr(input, token);
+    while (instruction != NULL) {
+        int parse_len = 0;
+        long a = 0;
+        long b = 0;
+        char left[4] = {0};
+        char right[4] = {0};
+
+        int parser_state = parser_none;
+        instruction += strlen(token); // Go to the end of the start token
+        while (
+               instruction != NULL &&
+               parse_len < MAX_INSTRUCTION_LEN + 1 &&
+               parser_state != parser_error &&
+               parser_state != parser_complete)
+        {
+            int digit_len = 0;
+
+            switch (parser_state) {
+                case parser_none:
+                    // Parse up to 3 digits and copy them into the left digit buffer
+                    while (digit_len < 3 && isdigit(instruction[digit_len]) > 0) {
+                        left[digit_len] = instruction[digit_len];
+                        digit_len++;
+                        parse_len++;
+                    }
+                    instruction+=digit_len;
+                    if (digit_len > 0) {
+                        parser_state = parser_left_digit;
+                    } else {
+                        // Break out of the loop and look for the next instruction token.
+                        parser_state = parser_error;
+                    }
+                    break;
+                case parser_left_digit:
+                    // Verify the current character is a comma
+                    if (instruction[0] == ',') {
+                        parser_state = parser_comma;
+                        instruction++;
+                        parse_len++;
+                    } else {
+                        // Break out of the loop and look for the next instruction token.
+                        parser_state = parser_error;
+                    }
+                    break;
+                case parser_comma:
+                    // Parse up to 3 digits and copy them into the right digit buffer
+                    while (digit_len < 3 && isdigit(instruction[digit_len]) > 0) {
+                        right[digit_len] = instruction[digit_len];
+                        digit_len++;
+                        parse_len++;
+                    }
+                    instruction+=digit_len;
+                    if (digit_len > 0) {
+                        parser_state = parser_right_digit;
+                    } else {
+                        // Break out of the loop and look for the next instruction token.
+                        parser_state = parser_error;
+                    }
+                    break;
+                case parser_right_digit:
+                    // Verify the current character is a close parenthesis
+                    if (instruction[0] == ')') {
+                        parser_state = parser_close_paren;
+                        instruction++;
+                        parse_len++;
+                        break;
+                    } else {
+                        parser_state = parser_error;
+                    }
+                    break;
+                case parser_close_paren:
+                    // We have a full, valid instruction. Multiply left and right values.
+                    a = atol(left);
+                    b = atol(right);
+                    total += a*b;
+                    parser_state = parser_complete;
+                    break;
+                default:
+                    // This should be impossible, since we set the state before entering this loop
+                    fprintf(stderr, "ERROR: An unexpected error occurred while parsing instructions. Default state reached.\n");
+                    parser_state = parser_error;
+            }
+        }
+
+        // Find the next instruction token
+        instruction = strstr(instruction, token);
+    }
+    return total;
 }
 
 /**
@@ -72,4 +183,11 @@ int main(int argc, char **argv) {
     char data_buf[pos + 1] = {};
     fread(data_buf, sizeof(char), pos, input_file);
     data_buf[pos + 1] = '\0';
+
+    long part1 = parse_instructions(data_buf);
+    printf("Part 1 Sum: %ld\n", part1);
+
+    recover_instructions(data_buf, sizeof(data_buf) / sizeof(data_buf[0]));
+    long part2 = parse_instructions(data_buf);
+    printf("Part 2 Sum: %ld\n", part2);
 }
